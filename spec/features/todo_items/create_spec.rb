758306a30e54516fcdd2b0db3776c3c8029b3cf0
Michael Nickey <mnickey@gmail.com>
2014-02-16 02:03:08 -0800

    added class item to td in index.html.erb todo_items
    added class item to td in index.html.erb todo_items

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe "Adding todo items" do
   let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
   
-  it "is sucessful with valid content" do
+  it "is successful with valid content" do
     visit_todo_list(todo_list)
     click_link "New Todo Item"
     fill_in "Content", with: "Milk"
     click_button "Save"
     expect(page).to have_content("Added todo list item")
-    within("ul.todo_items") do
+    within("td.item") do
       expect(page).to have_content("Milk")
     end
   end
